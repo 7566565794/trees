77b3c83ef40fbfa3312d5cbdc75650cbda90fa8f
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-11 11:38:35 +0530

    Update reverse_level_order_traversal.cpp

--- reverse_level_order_traversal.cpp
+++ reverse_level_order_traversal.cpp
@@ -1,1 +1,23 @@
-
+void reversePrint(Node *node)
+{
+   if(node==NULL)
+        return;
+    queue<Node *> q;
+    stack<Node *> s;
+    Node * curr=node;
+    q.push(curr);
+    while(q.empty()==false){
+        curr=q.front();
+        q.pop();
+        s.push(curr);
+        if(curr->right!=NULL)
+            q.push(curr->right);
+        if(curr->left!=NULL)
+            q.push(curr->left);
+    }
+    while(s.empty()==false){
+        Node *temp=s.top();
+        cout<<temp->data<< " ";
+        s.pop();
+    }
+}
