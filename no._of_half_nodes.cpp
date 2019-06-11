5a2eeb3e4984390f6e3b8f91c9aedd279e6b495c
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-11 12:04:05 +0530

    Create no._of_half_nodes.cpp

---
+++ no._of_half_nodes.cpp
@@ -0,0 +1,25 @@
+unsigned int gethalfCount(struct Node* node) 
+{ 
+    int count=0;
+    if(node==NULL)
+        return 0;
+    queue<Node *> q;
+    q.push(node);
+    while(!q.empty()){
+        Node *temp=q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        //************************************************************************
+        // In level order traversal, we have to just add below condition to solve this question 
+        if(temp->left == NULL && temp->right!=NULL  || temp->right == NULL && temp->left != NULL){
+            count++;
+        }
+        //*************************************************************************
+        if(temp->left!=NULL){
+            q.push(temp->left);}
+        if(temp->right!=NULL){
+            q.push(temp->right);
+        }
+    }
+    return count;
+} 
