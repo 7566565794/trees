810a6022ecec5bd67c4a1c5ecb940f91cdc626e9
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-06-12 12:16:31 +0530

    Create avarage_of_elements_at_each_level.cpp

---
+++ avarage_of_elements_at_each_level.cpp
@@ -0,0 +1,31 @@
+void averageOfLevels(Node* root) 
+{ 
+   if(root==NULL){
+       return;
+   }
+   queue<Node *> q;
+   q.push(root);
+
+   while(q.empty()==false){
+     queue<Node *>q1;
+     int count=0;
+     int sum=0;
+       while(q.empty()==false){
+           Node *temp;
+           temp=q.front();
+           q.pop();
+           sum=sum+temp->val;
+           count++;
+           if(temp->left!=NULL){
+               q1.push(temp->left);
+           }
+           if(temp->right!=NULL){
+               q1.push(temp->right);
+           }
+       }
+        q=q1;
+   cout << (sum * 1.0 / count) << " "; 
+   }
+  
+    
+} 
